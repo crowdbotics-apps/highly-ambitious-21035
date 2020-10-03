67a758e6d7746e2deaae1be66cd76680fda82d5d
Crowdbotics <team@crowdbotics.com>
2020-10-03 21:10:48 +0000

    Update models

--- backend/highly_ambitious_21035/wsgi.py
+++ backend/highly_ambitious_21035/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'highly_ambitious_21035.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "highly_ambitious_21035.settings")
 
 application = get_wsgi_application()
